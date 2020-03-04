91bf2a329aa933e2aabfdc89090263356c9a161c
horance <horance@outlook.com>
2020-03-04 20:55:55 +0800

    sprint 2

--- length_test.go
+++ length_test.go
@@ -9,3 +9,11 @@ func TestMile(t *testing.T) {
 	assert.True(t, Mile(3).Equal(Mile(3)))
 	assert.True(t, Mile(3).NotEqual(Mile(2)))
 }
+
+func TestMileAndYard(t *testing.T) {	
+	assert.True(t, Mile(1).Equal(Yard(1760)))
+	assert.True(t, Mile(1).NotEqual(Yard(1761)))
+	
+	assert.True(t, Yard(3).Equal(Yard(3)))
+	assert.True(t, Yard(3).NotEqual(Yard(4)))
+}
